c2579247402d088eb8c24cb789ae23580fd4fe2d
purushottam kapsikar <kapsikarpk@gmail.com>
2026-02-21 19:18:13 +0530

    fix category serializer for URLField

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Category, Product, Inquiry,Testimonial
 class CategorySerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
     class Meta:
         model = Category
         fields = [
