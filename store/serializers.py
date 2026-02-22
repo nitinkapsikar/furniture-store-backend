38dc37dfab2e7e167f56332380724b367260537a
purushottam kapsikar <kapsikarpk@gmail.com>
2026-02-22 17:25:26 +0530

    changed testimonial image to URLField

--- store/serializers.py
+++ store/serializers.py
@@ -52,14 +52,7 @@ class InquirySerializer(serializers.ModelSerializer):
         ]
 
 class TestimonialSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Testimonial
         fields = "__all__"
-
-    def get_image(self, obj):
-        request = self.context.get("request")
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
